6bb8dd016dc5b62e6739bae3aba392a3a4c8aa95
MeritStarz1 <merithstan4real@gmail.com>
2022-09-16 01:37:41 -0700

    love

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,30 +1,47 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - prints all possible combinations of two two-digit numbers
-* Return: Always 0 (Success)
-*/
-
-
-int main(void)
-{int first, second;
-for (first = 0; first < 100; first++)
+ * print_number -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
+ */
+void print_number(int n)
 {
-for (second = 0; second < 100; second++)
-{
-if (first < second)
-{putchar((first / 10) + 48);
-putchar((first % 10) + 48);
-putchar(' ');
-putchar((second / 10) + 48);
-putchar((second % 10) + 48);
-if (first != 98 || second != 99)
-{putchar(',');
-putchar(' ');
+	unsigned int z;
+	int m, b;
+
+	b = 10;
+
+	if (n < 10 && n >= 0)
+	{
+		_putchar (n + '0');
+	}
+	else if (n > -10 && n < 0)
+	{
+		n = n - 2 * n;
+		_putchar('-');
+		_putchar (n + '0');
+	}
+
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			_putchar ('-');
+		}
+		z = n;
+	while (z / b > 9)
+	{
+		b = b * 10;
+	}
+	while (b > 0)
+	{
+		m = z / b;
+		z = z % b;
+		_putchar (m + '0');
+		b = b / 10;
+	}
+	}
 }
-}
-}
-}
-putchar('\n');
-return (0);
-}
+
