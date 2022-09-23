1d5413769207dd416afc63e6a0557ba47c709fd2
MeritStarz1 <merithstan4real@gmail.com>
2022-09-23 14:45:18 -0700

    jug

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,18 +6,19 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	else if (n > 0)
-		n *= -1;
-	else
-		_putchar('-');
-	if ((n / 10) != 0)
-	{
-		print_number((n / 10) * -1);
-	}
-	_putchar((n % 10) * -1 + '0');
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+else if (n > 0)
+n *= -1;
+else
+_putchar('-');
+if ((n / 10) != 0)
+{
+		
+print_number((n / 10) * -1);
+}
+_putchar((n % 10) * -1 + '0');
 }
